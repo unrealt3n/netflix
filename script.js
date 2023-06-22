bbc7cec4e1c8a1e3022f1fd965dcff3b9a2b2a88
unrealt3n <xunrealt3n@gmail.com>
2023-06-22 13:29:38 +0530

    validation

--- script.js
+++ script.js
@@ -5,12 +5,23 @@ function onGetStart(e) {
     let input1 = document.querySelector('#email1Input').value;  
     let input2 = document.querySelector('#email2Input').value;
     let email = (e.target.id == 'email1') ? input1 : input2;
-    
-    if(email.includes('@' && '.com')){
-        alert( 'received your email '+ email);
-        document.querySelector('#email2Input').value = '';
-        document.querySelector('#email1Input').value = '';
-    }else{
-        alert( 'make sure form in filled and given a valid email');
-    }
+
+    let errorMsg1 = document.querySelector('#errorMsg1')
+    let errorMsg2 = document.querySelector('#errorMsg2')
+
+    let pattern =/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    if(email.length !=0 ){
+
+        if(pattern.test(email)){
+            alert( 'received your email '+ email);
+            document.querySelector('#email2Input').value = '';
+            document.querySelector('#email1Input').value = '';
+            email == input1 ? errorMsg1.style.display = "none" : errorMsg2.style.display = "none";
+            
+        }else{
+            email == input1 ? errorMsg1.style.display = "block"  : errorMsg2.style.display = "block" ;
+        }
+}else{
+    alert('email field cannot be blank')
+}
 }
